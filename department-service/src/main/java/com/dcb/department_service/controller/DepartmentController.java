fac5a6e6296c6acf275938fd5b5cf1261153832b
Shrikant Kale <kaleshrikant@live.com>
2025-03-04 15:42:43 +0530

    Updating department service with distribution tracing

---
+++ department-service/src/main/java/com/dcb/department_service/controller/DepartmentController.java
@@ -0,0 +1,36 @@
+package com.dcb.department_service.controller;
+
+import com.dcb.department_service.model.Department;
+import com.dcb.department_service.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @PostMapping
+    public Department add(@RequestBody Department department) {
+        log.info("Department add");
+        return departmentRepository.addDepartment(department);
+    }
+
+    @GetMapping
+    public List<Department> findAll() {
+        log.info("Department findAll");
+        return departmentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable Long id) {
+        log.info("Department findById");
+        return departmentRepository.findById(id);
+    }
+}
---
+++ department-service/src/main/java/com/dcb/department_service/model/Employee.java
@@ -0,0 +1,4 @@
+package com.dcb.department_service.model;
+
+public record Employee(Long departmentId, String name, int age, String position) {
+}
