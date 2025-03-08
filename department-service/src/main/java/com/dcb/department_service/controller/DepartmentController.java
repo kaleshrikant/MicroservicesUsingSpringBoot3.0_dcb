cf638a3970f4b54deec30d958f9c3485225f7065
Shrikant Kale <kaleshrikant@live.com>
2025-03-08 11:02:58 +0530

    updated DepartmentController to communicate with Employee service

--- department-service/src/main/java/com/dcb/department_service/controller/DepartmentController.java
+++ department-service/src/main/java/com/dcb/department_service/controller/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.dcb.department_service.controller;
 
+import com.dcb.department_service.client.EmployeeClient;
 import com.dcb.department_service.model.Department;
 import com.dcb.department_service.repository.DepartmentRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,9 @@ public class DepartmentController {
 
     @Autowired
     private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private EmployeeClient employeeClient;
 
     @PostMapping
     public Department add(@RequestBody Department department) {
@@ -32,5 +36,13 @@ public class DepartmentController {
     public Department findById(@PathVariable Long id) {
         log.info("Department findById");
         return departmentRepository.findById(id);
+    }
+
+    @GetMapping("/with-employee")
+    public List<Department> findAllWithEmployees() {
+        log.info("Department findAllWithEmployees");
+        List<Department> departments = departmentRepository.findAll();
+        departments.forEach(department -> department.setEmployees(employeeClient.findByDepartment(department.getId())));
+        return departments;
     }
 }
