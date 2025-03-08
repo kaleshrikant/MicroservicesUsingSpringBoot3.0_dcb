d354e919659aebbd925d7cd1331f52607875a2c7
Shrikant Kale <kaleshrikant@live.com>
2025-03-08 11:01:09 +0530

    updated EmployeeClient

--- department-service/src/main/java/com/dcb/department_service/client/EmployeeClient.java
+++ department-service/src/main/java/com/dcb/department_service/client/EmployeeClient.java
@@ -1,13 +1,14 @@
 package com.dcb.department_service.client;
 
 import com.dcb.department_service.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
 
 @HttpExchange
 public interface EmployeeClient {
-
-    @GetMapping("employee/department/{departmentId}")
-    public Employee findById(@PathVariable("departmentId") Long departmentId);
+    @GetExchange("/employee/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
 }
